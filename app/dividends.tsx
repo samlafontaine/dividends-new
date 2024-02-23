4cca1dde1c446ab8c93061ad042186d790ed39f9
samlafontaine <sam@samlafontaine.com>
2024-02-23 13:29:23 +0100

    init

---
+++ app/dividends.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface Dividend {
+  year: string;
+  months: { [month: string]: string };
+}
+
+interface Props {
+  data: {
+    "Monthly Adjusted Time Series": {
+      [date: string]: {
+        "7. dividend amount": string;
+      };
+    };
+  };
+}
+
+const DividendByMonth: React.FC<Props> = ({ data }) => {
+  const [dividendsByYear, setDividendsByYear] = useState<Dividend[]>([]);
+
+  useEffect(() => {
+    if (data?.["Monthly Adjusted Time Series"]) {
+      const monthlyData = data["Monthly Adjusted Time Series"];
+
+      // Grouping data by year
+      const groupedByYear: { [year: string]: { [month: string]: string } } = {};
+      // biome-ignore lint/complexity/noForEach: <explanation>
+      Object.keys(monthlyData).forEach((date) => {
+        const year = date.substring(0, 4);
+        const month = date.substring(5, 7);
+        if (!groupedByYear[year]) {
+          groupedByYear[year] = {};
+        }
+        groupedByYear[year][month] = monthlyData[date]["7. dividend amount"];
+      });
+
+      // Converting grouped data to array of Dividend objects
+      const dividends: Dividend[] = Object.keys(groupedByYear).map((year) => {
+        return {
+          year,
+          months: groupedByYear[year],
+        };
+      });
+
+      setDividendsByYear(dividends);
+    }
+  }, [data]);
+
+  return (
+    <div className="mt-6 w-full">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Year</TableHead>
+            <TableHead>Jan</TableHead>
+            <TableHead>Feb</TableHead>
+            <TableHead>Mar</TableHead>
+            <TableHead>Apr</TableHead>
+            <TableHead>May</TableHead>
+            <TableHead>Jun</TableHead>
+            <TableHead>Jul</TableHead>
+            <TableHead>Aug</TableHead>
+            <TableHead>Sep</TableHead>
+            <TableHead>Oct</TableHead>
+            <TableHead>Nov</TableHead>
+            <TableHead>Dec</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {dividendsByYear.toReversed().map((dividend, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <TableRow key={index}>
+              <TableCell>{dividend.year}</TableCell>
+              <TableCell>{dividend.months["01"] || "-"}</TableCell>
+              <TableCell>{dividend.months["02"] || "-"}</TableCell>
+              <TableCell>{dividend.months["03"] || "-"}</TableCell>
+              <TableCell>{dividend.months["04"] || "-"}</TableCell>
+              <TableCell>{dividend.months["05"] || "-"}</TableCell>
+              <TableCell>{dividend.months["06"] || "-"}</TableCell>
+              <TableCell>{dividend.months["07"] || "-"}</TableCell>
+              <TableCell>{dividend.months["08"] || "-"}</TableCell>
+              <TableCell>{dividend.months["09"] || "-"}</TableCell>
+              <TableCell>{dividend.months["10"] || "-"}</TableCell>
+              <TableCell>{dividend.months["11"] || "-"}</TableCell>
+              <TableCell>{dividend.months["12"] || "-"}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default DividendByMonth;
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="antialiased max-w-2xl mb-12 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
